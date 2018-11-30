8e1e8be03171d9661c29377a90bd69e018a5cdef
nfwinget <nflesher82@yahoo.com>
2018-11-29 20:49:50 -0600

    Initial commit

---
+++ src/Fraction.java
@@ -0,0 +1,47 @@
+public class Fraction {
+
+    public int getNumerator(){
+
+    }
+
+    public int getDenominator(){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public double toDouble(){
+
+    }
+
+    public Fraction add(Fraction other){
+
+    }
+
+    public Fraction subtract(Fraction other){
+
+    }
+
+    public Fraction multiply(Fraction other){
+
+    }
+
+    public Fraction divide(Fraction other){
+
+    }
+
+    public boolean equals(Object other){
+
+    }
+
+    public void toLowestTerms(){
+
+    }
+
+    public int gcd(int num, int den){
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]){
+
+    }
+
+    public String getOperation(Scanner input){
+
+    }
+
+    public boolean validFraction(String input){
+
+    }
+
+    public Fraction getFraction(Scanner input){
+
+    }
+}
