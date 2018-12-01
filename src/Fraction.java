e7f511c3a632a4f218527e228410f6119a94b2d6
nfwinget <nflesher82@yahoo.com>
2018-12-01 10:50:07 -0600

    Updated with comments from project description to help clarify tasks

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,46 +1,89 @@
 public class Fraction {
 
-    public int getNumerator(){
+    //FIELDS - two private instance variables to hold the numerator and denominator as ints
+    private int numerator;
+    private int denominator;
 
+    //CONSTRUCTOR - a two parameter constructor that initializes the numerator and denominator as ints
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        //thows an IllegalArgumentException if the denominator is 0
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        } else {
+            this.denominator = denominator;
+        }
+        //If the user enters a negative denominator bump the negative sign to the numerator. For example, -3/-2 should be
+        // converted to 3/2. Likewise, 5/-3 should be converted to -5/3
+        //example: Fraction myFrac = new Fraction(4,5); creates a fraction who's numerator is 4 and denominator is 5
+        //one parameter constructor that initializes the object equal in value to the integer parameter.
+        //example: Fraction myFrac = new Fraction(3) would create a Fraction with numerator equal to 3 and denominator equal to 1.
+        //zero parameter constructor that initializes the object to 0, meaning the numerator is 0 and the denominator is 1
+        //example: Fraction myFrac = new Fraction(); would create a fraction who's decimal value is 0
+        //You should eliminate as much redundancy as possible by letting your constructors rely on one another using the "this" keyword.
+    }
+
+    //BEHAVIORS
+    public int getNumerator(){
+        //exposes the value of the numerator field to the user
+        return numerator;
     }
 
     public int getDenominator(){
-
+        //exposes the value of the denominator field to the user
+        return denominator;
     }
 
     public String toString(){
+        //no parameters, returns a String, "numerator/denominator", a String representation of the Fraction
 
     }
 
     public double toDouble(){
+        //no parameters, returns a double, the result of numerator/denominator
 
     }
 
     public Fraction add(Fraction other){
+        //has parameters Fraction other, returns Fraction, returns a new Fraction that is the sum of other and this fraction
 
     }
 
     public Fraction subtract(Fraction other){
+        //has parameters Fraction other, returns Fraction, returns a new Fractions that is the difference between the other and this fraction
 
     }
 
     public Fraction multiply(Fraction other){
+        //has parameters Fraction other, returns Fraction, returns a new Fraction that is the product of the other and this fraction
 
     }
 
     public Fraction divide(Fraction other){
+        //has parameters Fraction other, returns Fraction, returns a new Fraction that is the division of the other and this fraction,
+        //throw an IllegalArgumentException() if the user asks you to divide by 0
 
     }
 
     public boolean equals(Object other){
+        //has parameters Object other, returns boolean, must take in an "Object" to properly override the Object class's equals method,
+        //but should ultimately check if two fractions are equal
 
     }
 
     public void toLowestTerms(){
+        //has no parameters, returns void, converts fraction to the lowest terms
+        //while a and b are not zero
+            //find the remainder of a divided by b
+            //set a to b
+            //set b to the remainder you found
+        //return a
 
     }
 
     public int gcd(int num, int den){
+        //has parameters int num int den, returns int, takes in two ints and determines the greatest common divisor of
+        //the two ints, should be a static method
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
 public class Main {
+
     public static void main(String args[]){
 
     }
 
     public String getOperation(Scanner input){
+        //has parameter Scanner input, returns String, Asks the user to enter in a valid mathematical operation. If the
+        //user enters anything except "+", "-", "/", "*", "=", "q", or "Q" it should re-prompt
+        //them until there is valid input.
 
     }
 
     public boolean validFraction(String input){
+        //has parameter String input, returns boolean, returns true if the parameter is in the form "a/b" where a is
+        //any int and b is any positive int
+        //Some things to be mindful of when implementing the validFraction() method:
+        //The first character may or may not be a "-" character. If a negative shows up anywhere else, then it is not a
+        //valid fraction. It may be helpful to
+        //remove the "-" character if there is one.
+        //If there is no "/" character, then every character in the string must be a number (if you removed the "-" sign).
+        //If there is a "/" character, then it may be helpful to create substrings for the numerator and denominator.
+        //Both substrings must be non-empty.
+        //Both must be entirely made of numbers.
+        //The denominator cannot be "0".
+        //Hint 1: It may be useful to create a helper method isNumber() that takes a String as input and returns true
+        //if every character in the String is a
+        //number 0-9 and false otherwise. This method can also check for
 
     }
 
     public Fraction getFraction(Scanner input){
+        //has parameter Scanner input, returns Fraction, It prompts the user for a String that is a validFraction.
+        //If they enter any thing that is not a valid Fraction, it should re-prompt them until it is valid
 
     }
 }
+
+//Putting it all together:
+//1. Write a short introduction method that describes the calculator program and welcomes your user
+//2. Ask the user to enter in an operation
+//3. As long as the user enters something that’s not "q" or "Q" when asked for an operation you should run the calculator
+//4. Get two fractions from the user and then perform whichever operation they ask for
+//5. Print the result of the operation
