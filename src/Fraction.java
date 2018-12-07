7471768c77b4620d3f589ae1f52d1503f0176af9
nfwinget <nflesher82@yahoo.com>
2018-12-06 21:58:45 -0600

    Updates 12/6/2018 9:58pm

--- src/Fraction.java
+++ src/Fraction.java
@@ -14,14 +14,14 @@ public class Fraction {
             this.denominator = denominator;
         }
 
-        //If the user enters a negative denominator bump the negative sign to the numerator. For example, -3/-2 should be
-        // converted to 3/2. Likewise, 5/-3 should be converted to -5/3
-        //example: Fraction myFrac = new Fraction(4,5); creates a fraction who's numerator is 4 and denominator is 5
-        //one parameter constructor that initializes the object equal in value to the integer parameter.
-        //example: Fraction myFrac = new Fraction(3) would create a Fraction with numerator equal to 3 and denominator equal to 1.
-        //zero parameter constructor that initializes the object to 0, meaning the numerator is 0 and the denominator is 1
-        //example: Fraction myFrac = new Fraction(); would create a fraction who's decimal value is 0
-        //You should eliminate as much redundancy as possible by letting your constructors rely on one another using the "this" keyword.
+        /*If the user enters a negative denominator bump the negative sign to the numerator. For example, -3/-2 should be
+        converted to 3/2. Likewise, 5/-3 should be converted to -5/3
+        example: Fraction myFrac = new Fraction(4,5); creates a fraction who's numerator is 4 and denominator is 5
+        one parameter constructor that initializes the object equal in value to the integer parameter.
+        example: Fraction myFrac = new Fraction(3) would create a Fraction with numerator equal to 3 and denominator equal to 1.
+        zero parameter constructor that initializes the object to 0, meaning the numerator is 0 and the denominator is 1
+        example: Fraction myFrac = new Fraction(); would create a fraction who's decimal value is 0
+        You should eliminate as much redundancy as possible by letting your constructors rely on one another using the "this" keyword.*/
     }
 
     //BEHAVIORS
--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,24 @@ public class Main {
         //4. Get two fractions from the user and then perform whichever operation they ask for
         Fraction fraction1 = new Main().getFraction();//have to make a new instance of the class to call a non-static
         Fraction fraction2 = new Main().getFraction();//method from a static method
-
+/*
+        if (operation.equals("+")) {
+            Fraction result = new Fraction.add(fraction1, fraction2);
+        } else if (operation.equals("-")) {
+            Fraction result = new Fraction.subtract(fraction1, fraction2);
+        } else if (operation.equals("*")) {
+            Fraction result = new Fraction.multiply(fraction1, fraction2);
+        } else if (operation.equals("/")) {
+            Fraction result = new Fraction.divide(fraction1, fraction2);
+        } else if (operation.equals("=")) {
+            boolean result = new Fraction.equals(fraction1, fraction2);
+        } else {
+            System.exit(0);
+        }
+*/
         System.out.println("Print results of " + fraction1.toString() + " " + operation + " " + fraction2.toString());
-
+        //System.out.println("Result = " + result);
         //5. Print the result of the operation
-
     }
 
     public static void intro() {
@@ -49,30 +62,70 @@ public class Main {
         return operation;
     }
 
-    public boolean validFraction(String frac){
-        //has parameter String input, returns boolean, returns true if the parameter is in the form "a/b" where a is
-        //any int and b is any positive int
+    public boolean isNumber(String string) {
+        /*loop through characters of string to see if it is a number 0-9, returns false if not
+        Hint 1: It may be useful to create a helper method isNumber() that takes a String as input and returns true
+        if every character in the String is a
+        number 0-9 and false otherwise. This method can also check forempty strings. Hint 2: Once you determine whether or not the Strings are
+        numbers, you may find the Integer.parseInt() method helpful.*/
+        for (int i = 1; i < string.length() + 1; i++) {
+            String string2 = string.substring(i-1,i);
+            if (string2.matches("[0-9]+")) {
+                //System.out.println(string2);
+                continue;
+            } else {
+                return false;
+            }
+        }
 
-        //Some things to be mindful of when implementing the validFraction() method:
-
-        //The first character may or may not be a "-" character. If a negative shows up anywhere else, then it is not a
-        //valid fraction. It may be helpful to
-        //remove the "-" character if there is one. USE IF .STARTSWITH("-") IS TRUE THEN...
-
-        //If there is no "/" character, then every character in the string must be a number (if you removed the "-" sign). COULD USE A FOR LOOP ITERATING OVER EACH CHARACTER TO HANDLE THIS
-
-        //If there is a "/" character, then it may be helpful to create substrings for the numerator and denominator.  TRY TO DO SOME SORT OF SPLIT
-
-        //Both substrings must be non-empty.
-        //Both must be entirely made of numbers.
-        //The denominator cannot be "0".
-        //Hint 1: It may be useful to create a helper method isNumber() that takes a String as input and returns true
-        //if every character in the String is a
-        //number 0-9 and false otherwise. This method can also check forempty strings. Hint 2: Once you determine whether or not the Strings are
-        //numbers, you may find the Integer.parseInt() method helpful.
-        if (frac == frac){
-            System.out.println("true");}
         return true;
+    }
+
+    public boolean validFraction(String frac){
+        /*has parameter String input, returns boolean, returns true if the parameter is in the form "a/b" where a is
+        any int and b is any positive int
+
+        Some things to be mindful of when implementing the validFraction() method:
+
+        The first character may or may not be a "-" character. If a negative shows up anywhere else, then it is not a
+        valid fraction. It may be helpful to remove the "-" character if there is one.*/
+        boolean bvalue = true;
+        String positiveFrac = "";
+        if (frac.startsWith("-")) {
+            positiveFrac = frac.substring(1, frac.length());
+        } else if (!frac.startsWith("-") && frac.contains("-")) {
+            bvalue = false;
+        }
+
+        /*If there is no "/" character, then every character in the string must be a number (if you removed the "-" sign).
+        If there is a "/" character, then it may be helpful to create substrings for the numerator and denominator.
+        Both substrings must be non-empty.
+        Both must be entirely made of numbers.
+        The denominator cannot be "0".*/
+        if (!positiveFrac.contains("/")) {
+            if (isNumber(positiveFrac)){
+                bvalue = true;
+            } else {
+                bvalue = false;
+            }
+        } else if (positiveFrac.contains("/")){
+            String[] numden = {};
+            numden = positiveFrac.split("/");
+            String numer = numden[0];
+            String denom = numden[1];
+
+            if (isNumber(numer) && isNumber(denom) && !denom.equals("0")){
+                bvalue = true;
+            } else {
+                bvalue = false;
+            }
+        }
+
+        return bvalue;
+        /*Hint 1: It may be useful to create a helper method isNumber() that takes a String as input and returns true
+        if every character in the String is a
+        number 0-9 and false otherwise. This method can also check forempty strings. Hint 2: Once you determine whether or not the Strings are
+        numbers, you may find the Integer.parseInt() method helpful.*/
     }
 
     public Fraction getFraction(){
